af098cd0ea73af1b47af0e789300d8484ef10a13
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-15 19:02:13 +0100

    refactored code to switch from AsyncTask to loader

--- app/src/main/java/com/example/android/quakeearth/EarthquakeLoader.java
+++ app/src/main/java/com/example/android/quakeearth/EarthquakeLoader.java
@@ -1,10 +1,11 @@
 package com.example.android.quakeearth;
 
+import android.content.AsyncTaskLoader;
 import android.content.Context;
 
 import java.util.List;
 
-public class EarthquakeLoader {
+public class EarthquakeLoader  extends AsyncTaskLoader<List<Earthquake>> {
 
     private static final String LOG_TAG = EarthquakeLoader.class.getName();
     private String mUrl;
