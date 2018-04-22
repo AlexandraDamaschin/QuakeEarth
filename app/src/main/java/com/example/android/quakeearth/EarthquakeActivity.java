7178fa47c08e0f7a15a678cc3c4fa51f116a86ca
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-22 11:16:05 +0100

    onCreateLoader instantiates and returns a new Loader for the given ID

--- app/src/main/java/com/example/android/quakeearth/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakeearth/EarthquakeActivity.java
@@ -2,10 +2,12 @@ package com.example.android.quakeearth;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -27,8 +29,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     private static final String LOG_TAG = EarthquakeActivity.class.getName();
 
     // URL for earthquake data from the USGS dataset
-    private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query";
     private EarthquakeAdapter mAdapter;
     //constant value for the loader id
     private static final int EARTHQUAKE_LOADER_ID = 1;
@@ -138,7 +139,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         //determine which item was selected and what action to take
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            //open the SettingsActivity via an intent
+            //
             Intent settingsIntent = new Intent(this, SettingsActivity.class);
             startActivity(settingsIntent);
             return true;
@@ -147,8 +148,26 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     }
 
     @Override
+    // onCreateLoader instantiates and returns a new Loader for the given ID
     public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
-        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+//        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, the `format=geojson`
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", "time");
+        // Return the completed uri `http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=10&minmag=minMagnitude&orderby=time
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
 
     @Override
